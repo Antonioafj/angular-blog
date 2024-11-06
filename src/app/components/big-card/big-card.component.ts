28aa5cab63634b430e1b7c0c7aea31ae3c708299
Antonioafj <antonioafj.edu@gmail.com>
2024-11-06 19:03:44 -0400

    criado compentente content datafake rotas

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './big-card.component.html',
   styleUrl: './big-card.component.css'
 })
-export class BigCardComponent {
+export class BigCardComponent implements OnInit{
+
+  @Input()
+  photoCover:string ="https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg"
+  
+  @Input()
+  cardTitle:string=""
+  
+  @Input()
+  cardDescription:string=""
+
+  @Input()
+  Id:string="0"
+
+  constructor(){}
+
+  ngOnInit(): void {}
+
+
 
 }
---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,16 @@
+export const dataFake = [
+    {
+        "id":"1",
+        "title": "Novo Homem De Ferro Em 3D ",
+        "description": "Marvel anuncia um novo filme do homem de ferro , confira",
+        "photoCover":"https://cdn.britannica.com/49/182849-050-4C7FE34F/scene-Iron-Man.jpg"
+
+    },
+    {
+        "id":"2",
+        "title": "Nova serie anunciada no Disney+ ",
+        "description": "Nova serie anunciada no DisneyNova serie anunciada no DisneyNova serie anunciada no DisneyNova serie anunciada no Disney",
+        "photoCover":"https://uploads.folhabv.com.br/2024/06/6O5N075Y-star-wars-1024x576.webp"
+
+    }
+] 
